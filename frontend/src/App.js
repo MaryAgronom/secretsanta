5250112019f8f2f6dcc6323d9754b249c80fa4b6
Gayanee <gayane.nalbandyan14@mail.ru>
2023-02-07 14:25:43 +0300

    eslint d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Check from './components/Check/Check';
 import Room from './components/Room/Room';
+import Giver from './components/Giver/Giver';
 import Layout from './components/Layout/Layout';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="check" element={<Check />} />
       </Route>
       <Route path="/rooms" element={<Room />}/>
+      <Route path="/giver" element={<Giver />}/>
       <Route path="*" element={<div>Error</div>} />
     </Routes>
   );
