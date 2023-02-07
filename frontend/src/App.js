bcfd92a8e518c1cf9814e23f4928451f05b9fbbd
Di <599371@gmail.com>
2023-02-07 14:01:38 +0300

    info about user and wishlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Check from './components/Check/Check';
 
 import Layout from './components/Layout/Layout';
+import UserRoom from './components/UserRoom/UserRoom';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="check" element={<Check />} />
       </Route>
+      <Route path="/user/room" element={<UserRoom />} />
       <Route path="*" element={<div>Error</div>} />
     </Routes>
   );
---
+++ frontend/src/components/UserRoom/UserMenu/UserMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function UserMenu() {
+  return (
+    <div className="userMenu">
+      <ul>
+        <li>Инфа о тебе</li>
+        <li>Инфа о подопечных</li>
+        <li>Подарки</li>
+      </ul>
+    </div>
+  );
+}
