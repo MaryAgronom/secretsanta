bd889f373c855d52438e214d7a0b9cc4fd9e08f5
Пользователь <maria.trksh@gmail.com>
2023-02-07 13:42:04 +0300

    nope

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Check from './components/Check/Check';
-
 import Layout from './components/Layout/Layout';
+import Room from './components/Adminroom/Adminroom';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="check" element={<Check />} />
       </Route>
+      <Route path="/room" element={<Room/>}/>
       <Route path="*" element={<div>Error</div>} />
     </Routes>
   );
