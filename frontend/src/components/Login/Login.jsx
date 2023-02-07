d019b7746497ee2cfd0a02d887acc42a439f204b
Di <599371@gmail.com>
2023-02-07 16:02:01 +0300

    lil fixes

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './ Login.module.css';
+import styles from './Login.module.css';
 export default function Login() {
   // const dispatch = useDispatch()
   const navigate = useNavigate();
